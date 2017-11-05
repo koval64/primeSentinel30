eda4a930138d034c0ce8f814b7f6267f9b0e72d4
marioc8 <marius.cichy@gmail.com>
2017-11-05 19:49:47 +0100

    Added 3 new results in MainApp description.

--- primeSentinel30/primeSentinel30/MainApp.java
+++ primeSentinel30/primeSentinel30/MainApp.java
@@ -3,6 +3,12 @@ package primeSentinel30;
 import java.text.NumberFormat;
 
 /** 
+ * Intel(R) Core(TM) i5-7600K CPU @ 3.80GHz
+ * 
+ *   64_410_000_030    128.697 s	2,701,477,379 primes	- max working value
+ *   50_000_000_010     97.805 s    2,119,654,578 primes
+ *   10_000_000_020     17.035 s      455,052,512 primes
+ * 
  * Intel(R) Core(TM) i5-7600K CPU @ 3.80GHz with OC 4.3GHz
  * 
  *	    100_000_020		 0.044 s
@@ -14,9 +20,8 @@ import java.text.NumberFormat;
  *	  4_780_000_020		 5.629 s	about 225 * 10^6 primes
  *	  5_000_000_010		 5.909 s	about 234 * 10^6  primes
  *	 50_000_000_010		60.537 s	2,119,654,578 primes
- *	 64_400_000_000		89.428 s	max working value, 2,701,075,683 primes
+ *	 64_400_000_000		89.428 s	2,701,075,683 primes
  *					   104.667 s	worst time
- * 
  */
 
 public class MainApp {
@@ -26,7 +31,8 @@ public class MainApp {
 	//private static final long MAX_VALUE = 9_699_690;	/*	2,3,5,7,11,13,17,19   wheel	*/
 	//private static final long MAX_VALUE = 64_400_000_000L;
 	//private static final long MAX_VALUE = 30030;
-	private static final long MAX_VALUE = 1_000_000_000L;
+	//private static final long MAX_VALUE = 64_410_000_000L;
+	private static final long MAX_VALUE = 10_000_000_000L;
 
 	private static void printInfo(PrimesSentinel sentinel){
 		String range = NumberFormat.getInstance().format( (MAX_VALUE/30+1) * 30 );
@@ -39,6 +45,7 @@ public class MainApp {
 
 		PrimesSentinel sentinel = new PrimesSentinel(MAX_VALUE);
 		printInfo(sentinel);
+		//System.out.println(Long.MAX_VALUE);
 		//sentinel.printPossiblePrimes();
 		//sentinel.printCols(30, 500);
 	}
